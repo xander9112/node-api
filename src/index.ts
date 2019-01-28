ffe43253837febcb88bce66fb88d7cf57b4fe6de
Alexander Furmanchuk <afurmanchuk@neoflex.ru>
2019-01-28 10:55:48 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,32 +2,41 @@ import app from './App'
 import {MongoClient, ObjectID} from 'mongodb'
 import * as passport from 'passport'
 import {Strategy} from 'passport-http-bearer'
+import {Strategy as JwtStrategy, ExtractJwt} from 'passport-jwt'
 import {decode} from 'jwt-simple'
-
-const port = process.env.PORT || 8000
 import db from './config/db'
 import config from './config'
 
+const port = process.env.PORT || 8000
+
+
+const options = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: 'secret',
+    issuer: 'accounts.insta.com',
+    audience: 'insta.com'
+}
 
 MongoClient.connect(db.url, {useNewUrlParser: true}, (err, client) => {
     if (err) return console.log(err)
 
     const db = client.db('node-api')
 
-    passport.use(new Strategy((token, done) => {
-        try {
-            const {_id} = decode(token, config.SECRET);
-
-            db.collection('users').findOne({'_id': new ObjectID(_id)}, (err) => {
-                if (err) {
-                    done(null, false);
-                } else {
-                    done(null, _id);
-                }
-            });
-        } catch (error) {
-            done(null, false);
-        }
+    passport.use(new JwtStrategy(options, (jwt_payload, done) => {
+        console.log(jwt_payload)
+        // try {
+        //     const {_id} = decode(token, config.SECRET);
+        //
+        //     db.collection('users').findOne({'_id': new ObjectID(_id)}, (err) => {
+        //         if (err) {
+        //             done(null, false);
+        //         } else {
+        //             done(null, _id);
+        //         }
+        //     });
+        // } catch (error) {
+        //     done(null, false);
+        // }
     }));
 
     app.mountRoutes(db)
--- src/routes/note-routes/index.ts
+++ src/routes/note-routes/index.ts
@@ -7,9 +7,9 @@ import {remove} from './remove'
 import {updateOne} from './updateOne'
 
 export default (app, db) => {
-    app.get('/notes', passport.authenticate('bearer', {session: false}), find(db));
-    app.get('/notes/:id', passport.authenticate('bearer', {session: false}), findOne(db));
-    app.delete('/notes/:id', passport.authenticate('bearer', {session: false}), remove(db));
-    app.put('/notes/:id', passport.authenticate('bearer', {session: false}), updateOne(db));
-    app.post('/notes', passport.authenticate('bearer', {session: false}), insertOne(db));
+    app.get('/notes', passport.authenticate('jwt', {session: false}), find(db));
+    app.get('/notes/:id', passport.authenticate('jwt', {session: false}), findOne(db));
+    app.delete('/notes/:id', passport.authenticate('jwt', {session: false}), remove(db));
+    app.put('/notes/:id', passport.authenticate('jwt', {session: false}), updateOne(db));
+    app.post('/notes', passport.authenticate('jwt', {session: false}), insertOne(db));
 };
